ac1e503f2cd8fea0caaa46224757867041938f8e
Ekaterina <penelope_v@mail.ru>
2021-11-30 08:22:50 +0300

    Using Angular Services and HttpClient to retrieve data

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,6 +1,8 @@
+import { ThrowStmt } from '@angular/compiler'
 import { Component, OnInit } from '@angular/core'
 
 import { ICurrentWeather } from '../interfaces'
+import { WeatherService } from '../weather/weather.service'
 
 @Component({
   selector: 'app-current-weather',
@@ -10,16 +12,20 @@ import { ICurrentWeather } from '../interfaces'
 export class CurrentWeatherComponent implements OnInit {
   current: ICurrentWeather
 
-  constructor() {
+  constructor(private weatherService: WeatherService) {
     this.current = {
       city: 'Bethesda',
       country: 'US',
-      date: new Date(),
+      date: 10,
       image: 'assets/img/sunny.svg',
-      temperature: 32,
+      temperature: 72,
       description: 'sunny',
     } as ICurrentWeather
   }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.weatherService
+      .getCurrentWeather('London', 'UK')
+      .subscribe((data) => (this.current = data))
+  }
 }
